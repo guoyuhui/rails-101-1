7bd072bb43edd19fd4021cce1ef1e6c0b98a2de0
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2016-12-14 01:15:12 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,10 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
     @group = Group.new
   end
 
- def show
-   @group = Group.find(params[:id])
- end
+  def show
+    @group = Group.find(params[:id])
+    @posts = @group.posts
+  end
 
  def edit
 
@@ -40,7 +41,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
  end
 
  def destroy
-   
+
 
    @group.destroy
    redirect_to groups_path, alert: "Group deleted"
