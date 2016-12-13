614c3d309edb4bb030e26e14f8978b891ce7c308
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2016-12-14 00:58:12 +0800

    add post and hook group/user relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :groups
-
+  has_many :groups
+  has_many :posts
 end
